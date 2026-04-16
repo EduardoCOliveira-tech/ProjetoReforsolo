2bef97381a75c73ae3ca553317b3dff3837ed19c
Eduardo notebook <eduardocarvalhooliveira2@gmail.com>
2026-04-15 21:54:21 -0300

    Fix: Removendo defaultViewport para passar no build da Vercel

--- app/api/pdf/route.ts
+++ app/api/pdf/route.ts
@@ -37,10 +37,8 @@ export async function POST(req: Request) {
       });
     } else {
       // Configuração para Produção (Vercel)
-      // Configuração para Produção (Vercel)
       browser = await puppeteer.launch({
         args: chromium.args,
-        defaultViewport: chromium.defaultViewport as any, // <- Adicione 'as any' aqui
         executablePath: await chromium.executablePath(),
         headless: true,
       });
